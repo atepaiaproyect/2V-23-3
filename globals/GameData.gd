7c3360c31addfae30b3323a8c961f22742444132
2V-23-3 <atepaiaproyect@gmail.com>
2026-03-23 15:55:23 -0300

    TROLO

--- globals/GameData.gd
+++ globals/GameData.gd
@@ -1,15 +1,12 @@
-# GameData.gd
-# Pizarra global que existe durante toda la sesión del juego.
-
 extends Node
 
-# --- Datos del jugador actual ---
+# --- Datos del jugador ---
 var player_name: String = ""
 var player_id: String = ""
 var player_email: String = ""
 var is_logged_in: bool = false
 
-# --- Configuración de Firebase ---
+# --- Firebase ---
 const FIREBASE_API_KEY = "AIzaSyAhAVBHtt71Emoa_4ohUa06Y_hQvEJWllM"
 const FIREBASE_PROJECT_ID = "atepaia-2v"
 const FIREBASE_AUTH_URL = "https://identitytoolkit.googleapis.com/v1/accounts:"
